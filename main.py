fb450535842657edadef42790336621614bc4d7d
Kishen97 <43293778+Kishen97@users.noreply.github.com>
2018-09-18 01:35:56 +0200

    Push button interrupt added
    defined the interrupt for the push buttons

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+#interrupt configured for the push buttons
+def init_pushbuttons():
+	GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
